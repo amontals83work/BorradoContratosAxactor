2b6bab20b8a7bf2e230939d4abe2f813f6a63dc3
Alejandro Montals Pozzoli <amontals83.work@gmail.com>
2024-02-27 16:26:49 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtIdContrato = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.BackColor = System.Drawing.Color.Red
+        Me.btnBorrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnBorrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBorrar.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnBorrar.Location = New System.Drawing.Point(27, 126)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(226, 31)
+        Me.btnBorrar.TabIndex = 0
+        Me.btnBorrar.Text = "BORRAR"
+        Me.btnBorrar.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(46, 92)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 15)
+        Me.Label3.TabIndex = 51
+        Me.Label3.Text = "IdContrato"
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 19)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(251, 48)
+        Me.Label4.TabIndex = 50
+        Me.Label4.Text = "BORRADO CONTRATOS AXACTOR"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtIdContrato
+        '
+        Me.txtIdContrato.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtIdContrato.Location = New System.Drawing.Point(116, 83)
+        Me.txtIdContrato.Name = "txtIdContrato"
+        Me.txtIdContrato.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.txtIdContrato.Size = New System.Drawing.Size(106, 29)
+        Me.txtIdContrato.TabIndex = 52
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(275, 180)
+        Me.Controls.Add(Me.txtIdContrato)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtIdContrato As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private mc As New MCCommand
+    Private openFileDialog As New OpenFileDialog
+
+    Public Sub Form1()
+        InitializeComponent()
+        CenterToScreen()
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+        Borrar()
+    End Sub
+
+    Private Sub Borrar()
+
+        Dim idContrato As String = txtIdContrato.Text.Trim
+
+        Try
+            If IsNumeric(idContrato) Then
+                mc.CommandText = "SELECT COUNT(*) FROM AXACONTRATOS WHERE idcontratoald = " & idContrato.ToString & ""
+                Dim count As Integer = mc.ExecuteScalar()
+
+                If count > 0 Then
+                    mc.CommandText = "SELECT fechaBaja FROM AXACONTRATOS WHERE idcontratoald = " & idContrato.ToString & ""
+                    Dim fechaBaja As String = mc.ExecuteScalar().ToString
+                    If fechaBaja = "" Then
+                        mc.CommandText = "UPDATE AXACONTRATOS SET fechaBaja = GETDATE() WHERE idcontratoald = " & idContrato.ToString & ""
+                        mc.ExecuteNonQuery()
+                        MessageBox.Show("BORRADO: " & idContrato.ToString)
+                    Else
+                        MessageBox.Show("LA BAJA DE " & idContrato.ToString & " FUE HECHA EL " & fechaBaja)
+                    End If
+                Else
+                    MessageBox.Show("NO EXISTE: " & idContrato.ToString)
+                End If
+            Else
+                MessageBox.Show("NO ES UN NÚMERO: " & idContrato.ToString)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("ERROR al procesar la solicitud: " & vbCrLf & ex.Message)
+        End Try
+
+    End Sub
+
+End Class
